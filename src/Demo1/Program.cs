2d0ee09b81caf9d5e3482d971d9a4865d0ece93e
S. Kyle Korndoerfer <kyle.korndoerfer@xerox.com>
2017-09-26 11:08:25 -0400

    Demo 1 complete; basic hashing

---
+++ src/Demo1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Demo1
+{
+	class Program
+	{
+		static void Main( string[] args )
+		{
+			// encode message as a byte array
+			var messageBytes = Encoding.Unicode.GetBytes( "TEST" );
+
+			// calculate the hash
+			byte[] hashBytes;
+			using ( var md5 = new MD5CryptoServiceProvider() )
+			{
+				hashBytes = md5.ComputeHash( messageBytes );
+				md5.Clear();
+			}
+
+			// output as a hex string
+			for ( int i = 0; i < hashBytes.Length; i++ )
+			{
+				Console.Write( hashBytes[i].ToString( "x2" ) );
+			}
+			// OUTPUT: 70659f64a0d9277bb3d15f5215ba50ab
+
+			Console.ReadKey();
+		}
+	}
+}

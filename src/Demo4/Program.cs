f3d70327aa3634845188f0409b59acf5ed3070fe
S. Kyle Korndoerfer <kyle.korndoerfer@xerox.com>
2017-09-26 11:27:22 -0400

    Completed Demo 4; HMAC

---
+++ src/Demo4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Demo4
+{
+	class Program
+	{
+		const string message = "Hello VDUNY!";
+
+		static void Main( string[] args )
+		{
+			// generate a key; 32 bytes * 8 bits/byte = 256 bits
+			// should be a pre-generated value with high entropy
+			var key = GenerateKey();
+
+			// calculate HMAC
+			byte[] hash;
+			using ( var hmac = new HMACSHA256( key ) )
+			{
+				var messageBytes = Encoding.Unicode.GetBytes( message );
+				hash = hmac.ComputeHash( messageBytes );
+				hmac.Clear();
+			}
+
+			Console.WriteLine( Convert.ToBase64String( hash ) );
+			// SAMPLE OUTPUT: qNW++z2JXsuydyhNm+SKg7XvNv9sAZILXcK2Azp9Bco=
+
+			Console.ReadKey();
+		}
+
+		static byte[] GenerateKey()
+		{
+			var key = new byte[32];
+			using ( var rng = new RNGCryptoServiceProvider() )
+			{
+				rng.GetBytes( key );
+			}
+
+			return key;
+		}
+	}
+}
